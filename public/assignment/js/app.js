c024fd283411d63ff5412ba27e84712687c12312
kreenasmehta <kreena@ccs.neu.edu>
2016-09-22 15:48:52 -0400

    added new pages, links and styling

---
+++ public/assignment/js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    // $( window ).on( "orientationchange", function( event ) {
+    //     if(window.innerHeight > window.innerWidth){
+    //         alert("dedbek");
+    //         $ ("#left-pane-landscape").load("website-list.html");
+    //     } else {
+    //         $ ("#left-pane-landscape").hide();
+    //     }
+    // });
+});

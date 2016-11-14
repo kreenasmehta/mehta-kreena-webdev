c16fe3a1ef358a81d94a9d93c20456d4707ab7b8
kreenasmehta <kreena@ccs.neu.edu>
2016-11-14 17:18:06 -0500

    mlab database connection

--- assignment/model/models.server.js
+++ assignment/model/models.server.js
@@ -3,8 +3,10 @@
  */
 module.exports = function () {
 
+    var connectionString ='mongodb://localhost/wam-fall-2016' || 'mongodb://heroku_m7fj4xln:8Unique#@ds035796.mlab.com:35796/heroku_m7fj4xln';
+
     var mongoose = require('mongoose');
-    mongoose.connect('mongodb://localhost/wam-fall-2016');
+    mongoose.connect(connectionString);
 
     var userModel = require("./user/user.model.server.js")();
 

f2f2a9e618357f9398687ecb8c193ddd762f108b
kreenasmehta <kreena@ccs.neu.edu>
2016-11-15 09:13:15 -0500

    temporarily removed dependency on model

--- assignment/app.js
+++ assignment/app.js
@@ -2,8 +2,8 @@
  * Created by kreenamehta on 10/24/16.
  */
 module.exports = function (app) {
-  var model = require("./model/models.server.js")();
-  require("./services/user.service.server.js")(app, model);
+  // var model = require("./model/models.server.js")();
+  require("./services/user.service.server.js")(app);
   require("./services/website.service.server.js")(app);
   require("./services/page.service.server.js")(app);
   require("./services/widget.service.server.js")(app);

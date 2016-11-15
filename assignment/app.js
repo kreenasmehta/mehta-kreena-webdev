67f53348f74c3e5ad728d8518bef45efc7c37c66
kreenasmehta <kreena@ccs.neu.edu>
2016-11-15 13:56:56 -0500

    added model to assignments with connectionString = localhost || mongoDB URI

--- assignment/app.js
+++ assignment/app.js
@@ -2,7 +2,7 @@
  * Created by kreenamehta on 10/24/16.
  */
 module.exports = function (app) {
-  // var model = require("./model/models.server.js")();
+  var model = require("./model/models.server.js")();
   require("./services/user.service.server.js")(app);
   require("./services/website.service.server.js")(app);
   require("./services/page.service.server.js")(app);
--- assignment/model/models.server.js
+++ assignment/model/models.server.js
@@ -3,7 +3,11 @@
  */
 module.exports = function () {
 
-    var connectionString ='mongodb://localhost/wam-fall-2016' || 'mongodb://heroku_m7fj4xln:8Unique#@ds035796.mlab.com:35796/heroku_m7fj4xln';
+    // var connectionString ='mongodb://localhost/wam-fall-2016';
+    var connectionString = process.env.MONGODB_URI || 'mongodb://localhost/wam-fall-2016';
+
+    // || 'mongodb://heroku_m7fj4xln:8Unique#@ds035796.mlab.com:35796/heroku_m7fj4xln';
+
 
     var mongoose = require('mongoose');
     mongoose.connect(connectionString);

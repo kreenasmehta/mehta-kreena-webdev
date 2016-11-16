bb1b5025b2434f692ef027edd51f4b10a96bddc6
kreenasmehta <kreena@ccs.neu.edu>
2016-11-16 18:32:03 -0500

    removed static arrays from user, website, page, widget

--- assignment/services/page.service.server.js
+++ assignment/services/page.service.server.js
@@ -3,12 +3,6 @@
  */
 module.exports = function (app, model) {
 
-    var pages = [
-        { "_id": "321", "name": "Post 1", "websiteId": "456", "description": "Lorem" },
-        { "_id": "432", "name": "Post 2", "websiteId": "456", "description": "Lorem" },
-        { "_id": "543", "name": "Post 3", "websiteId": "456", "description": "Lorem" }
-    ];
-
     app.post('/api/website/:wid/page', createPage);
     app.get('/api/website/:wid/page', findAllPagesForWebsite);
     app.get('/api/page/:pid', findPageById);
--- assignment/services/website.service.server.js
+++ assignment/services/website.service.server.js
@@ -3,15 +3,6 @@
  */
 module.exports = function (app, model) {
 
-    var websites = [
-        { "_id": "123", "name": "Facebook",    "developerId": "456", "description": "Lorem Facebook" },
-        { "_id": "234", "name": "Tweeter",     "developerId": "456", "description": "Lorem Tweeter" },
-        { "_id": "456", "name": "Gizmodo",     "developerId": "456", "description": "Lorem Gizmodo" },
-        { "_id": "567", "name": "Tic Tac Toe", "developerId": "123", "description": "Lorem Tic Tac Toe" },
-        { "_id": "678", "name": "Checkers",    "developerId": "123", "description": "Lorem Checkers" },
-        { "_id": "789", "name": "Chess",       "developerId": "234", "description": "Lorem Chess" }
-    ];
-
     app.post('/api/user/:uid/website', createWebsite);
     app.get('/api/user/:uid/website', findAllWebsitesForUser);
     app.get('/api/website/:wid', findWebsiteById);

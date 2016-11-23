1e740dd5319cdc83c0fafd0f0c8d05ea3109cd1d
kreenasmehta <kreena@ccs.neu.edu>
2016-11-23 09:34:28 -0500

    removed project server dependency

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static(__dirname + '/public'));
 // require ("./test/app.js")(app);
 
 require("./assignment/app.js")(app);
-require("./project/app.js")(app);
+// require("./project/app.js")(app);
 // require("./project/todo/app")(app);
 
 app.set('ipaddress', (process.env.IP));
